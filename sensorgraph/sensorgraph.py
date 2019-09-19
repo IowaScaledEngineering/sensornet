07258340dce5d87cca0e73dde1072e71b5418bc9
Nathan D. Holmes <ndholmes@fedex.com>
2019-09-19 16:04:04 -0600

    Made sensorgraph actually plot something returned by sensornexus

--- sensorgraph/sensorgraph.py
+++ sensorgraph/sensorgraph.py
@@ -4,12 +4,22 @@ import configparser
 import json
 import iso8601
 import matplotlib.pyplot as plt 
+import sys
 
 def main(config):
   baseURL = 'http://localhost:8082/gethistory'
   sensor = 'work/basement/temperature'
   url = '%s/%s' % (baseURL, sensor)
-  response = requests.get(url)
+
+  reqargs = { }  
+  reqargs['start'] = '2019-09-10T00:00:00Z'
+  reqargs['end'] = '2019-09-20T00:00:00Z'  
+  reqargs['increment'] = 60  
+  response = requests.get(url, reqargs)
+
+  if response.status_code != 200:
+    print("Failure - status code %d" % (response.status_code))
+    sys.exit()
   
   #print(response.json())
   
@@ -22,10 +32,11 @@ def main(config):
     x.append(measurementDT)
     y.append(float(element['value']))
 
-  plt.plot(x, y)   
+  plt.plot(x, y)
   plt.xlabel('Time') 
   plt.ylabel('Deg C') 
   plt.title('My first graph!') 
+  plt.ylim(0, 35)     # set the ylim to bottom, top
   plt.savefig("test.png")  
 
 if __name__== "__main__":
