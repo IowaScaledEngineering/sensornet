4fead2b63ac7a93d5d387923332edeb59f54495a
Nathan D. Holmes <maverick@drgw.net>
2019-09-22 15:14:50 -0600

    Refactor sensor graph to make it easier to grab multiple data series from sensornexus

--- sensorgraph/sensorgraph.py
+++ sensorgraph/sensorgraph.py
@@ -21,14 +21,74 @@ def find_le(a, x):
         return i-1
     raise ValueError
 
-def main(config):
+def generateXAxisTimes(startDT, endDT, increment):
+  x = [ ]
+  # Normalize time to whole minutes
+  incDT = startDT.astimezone(datetime.timezone.utc)
+  endDTUTC = endDT.astimezone(datetime.timezone.utc)
+  # We want roughly 2000 datapoints
+  while incDT < endDTUTC:
+    x.append(incDT)
+    incDT = incDT + datetime.timedelta(seconds=increment)
+  return x
+
+def convertCtoF(temp_c):
+  return (temp_c * 1.8) + 32
+
+def fetchSensor(startDT, endDT, increment, xAxis, sensorName, conversionFunc=None):
   baseURL = 'http://localhost:8082/gethistory'
-  sensor = 'work/basement/temperature'
-  url = '%s/%s' % (baseURL, sensor)
+  url = '%s/%s' % (baseURL, sensorName)
+  y = [ ]
+  reqargs = { }
+  reqargs['start'] = startDT.astimezone(datetime.timezone.utc).isoformat()
+  reqargs['end'] = endDT.astimezone(datetime.timezone.utc).isoformat()
+  reqargs['increment'] = increment  
+  response = requests.get(url, reqargs)
+
+  if response.status_code != 200:
+    print("Failure - status code %d" % (response.status_code))
+    # Fill retval with Nones
+    for xTime in xAxis:
+      y.append(None)
+    return
+  
+  data = response.json()
+
+  dtIndexes = []
+  for element in data:
+    dtIndexes.append(int(iso8601.parse_date(element['time']).astimezone(datetime.timezone.utc).timestamp()))
+
+  for incDT in xAxis:
+    val = None
+    potentialVal = None
+    potentialValDT = None
+
+    try:
+      potentialIdx = find_le(dtIndexes, int(incDT.timestamp()))
+      val = float(data[potentialIdx]['value'])
+    except Exception as e:
+      pass
+    if val is None:
+      y.append(val)
+    elif conversionFunc is not None:
+      try:
+        potentialVal = conversionFunc(val)
+      except:
+        potentialVal = val
+      y.append(potentialVal)
+    else:
+      y.append(val)
+  
+  return y
+
+def main(config):
+
   zone = 'America/Denver'
   tz = pytz.timezone(zone)
+  plt.rcParams['timezone'] = zone
+
   endDT = rounder(datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc))
-  startDT = endDT - datetime.timedelta(days=7)
+  startDT = endDT - datetime.timedelta(days=1)
   
   print("Starting DT: %s" % (startDT))
   print("Ending DT: %s" % (endDT))
@@ -39,52 +99,20 @@ def main(config):
 
   print("totalSeconds = %d, seconds per datapoint = %d" % (totalSeconds, secondsPerDatapoint))
 
-  reqargs = { }
-  reqargs['start'] = startDT.astimezone(datetime.timezone.utc).isoformat()
-  reqargs['end'] = endDT.astimezone(datetime.timezone.utc).isoformat()
-  reqargs['increment'] = secondsPerDatapoint  
-  response = requests.get(url, reqargs)
+  x = generateXAxisTimes(startDT, endDT, secondsPerDatapoint)
+  y = fetchSensor(startDT, endDT, secondsPerDatapoint, x, 'house/basement/temperature', conversionFunc=convertCtoF)
+  y2 = fetchSensor(startDT, endDT, secondsPerDatapoint, x, 'house/insulator_room/temperature', conversionFunc=convertCtoF)
 
-  if response.status_code != 200:
-    print("Failure - status code %d" % (response.status_code))
-    sys.exit()
-  
-  data = response.json()
-  x = [ ]
-  y = [ ]
-
-  dtIndexes = []
-  for element in data:
-    dtIndexes.append(int(iso8601.parse_date(element['time']).astimezone(datetime.timezone.utc).timestamp()))
-
-  # Normalize time to whole minutes
-  incDT = startDT.astimezone(datetime.timezone.utc)
-  endDTUTC = endDT.astimezone(datetime.timezone.utc)
-  
-  # We want roughly 2000 datapoints
-  while incDT < endDTUTC:
-    x.append(incDT)
-    val = None
-    potentialVal = None
-    potentialValDT = None
-
-    try:
-      potentialIdx = find_le(dtIndexes, int(incDT.timestamp()))
-      val = float(data[potentialIdx]['value'])
-    except Exception as e:
-      pass
-
-    y.append(val)
-
-    incDT = incDT + datetime.timedelta(seconds=secondsPerDatapoint)
 
   print("Starting graph generation")
   #sys.exit()
   fig, ax = plt.subplots()
-  ax.format_xdata = mdates.DateFormatter('%m/%d %H:%M')
-  fig.autofmt_xdate()
+  ax.format_xdata = mdates.DateFormatter('%H:%M')
   ax.xaxis.set_minor_locator(mdates.HourLocator())
-  plt.plot(x, y)
+  ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d %H:%M'))
+  fig.autofmt_xdate()
+  plt.plot(x, y, color='red')
+  plt.plot(x, y2)
   
   ticks = []
   labels = []
@@ -99,8 +127,9 @@ def main(config):
   plt.xlabel('Time') 
   plt.ylabel('Deg C') 
   plt.title('My first graph!') 
-  plt.ylim(0, 35)     # set the ylim to bottom, top
+  plt.ylim(0, 100)     # set the ylim to bottom, top
   plt.savefig("test.png")  
+  print("Completed graph generation")
 
 if __name__== "__main__":
   ap = argparse.ArgumentParser()
